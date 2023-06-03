53811c9abd42a763c58e5e684a38a45bc177e87b
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-06-03 22:02:57 +0300

    Added post for BreweryClient and related test

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
@@ -21,8 +22,12 @@ public class BreweryClient {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public BeerDto getBeerById(UUID uuid){
+    public BeerDto getBeerById(UUID uuid) {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
     }
 
     public void setApihost(String apihost) {
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +21,15 @@ class BreweryClientTest {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
 
+
+    @Test
+    void saveNewBeer() {
+        BeerDto dto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(dto);
+
+        assertNotNull(uri);
     }
 }
